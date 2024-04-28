4a4136ef1ac9600aa4a360e9f3e14467652c0e07
Jacob Hilker <jacob.hilker2@gmail.com>
2024-04-28 15:12:23 -0400

    started flake outputs

--- flake.nix
+++ flake.nix
@@ -1,15 +1,33 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-        # The name "snowfall-lib" is required due to how Snowfall Lib processes your
-        # flake's inputs.
-        snowfall-lib = {
-            url = "github:snowfallorg/lib";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+    # The name "snowfall-lib" is required due to how Snowfall Lib processes your
+    # flake's inputs.
+    snowfall-lib = {
+      url = "github:snowfallorg/lib";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    # We will handle this in the next section.
-    outputs = inputs: {};
+  # We will handle this in the next section.
+  outputs = inputs:
+    inputs.snowfall-lib.mkFlake {
+      # You must provide our flake inputs to Snowfall Lib.
+      inherit inputs;
+
+      # The `src` must be the root of the flake. See configuration
+      # in the next section for information on how you can move your
+      # Nix files to a separate directory.
+      src = ./.;
+
+      namespace = "jhilker98-dotfiles";
+      meta = {
+        # A slug to use in documentation when displaying things like file paths.
+        name = "jhilker98-dotfiles";
+
+        # A title to show for your flake, typically the name.
+        title = "Jacob's NixOS Flake";
+      };
+    };
 }
