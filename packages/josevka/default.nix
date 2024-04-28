cf53a8fd137ab645180b53a670ec2b135053f401
Jacob Hilker <jacob.hilker2@gmail.com>
2024-04-28 18:09:26 -0400

    added josevka

---
+++ packages/josevka/default.nix
@@ -0,0 +1,15 @@
+{
+# Snowfall Lib provides a customized `lib` instance with access to your flake's library
+# as well as the libraries available from your flake's inputs.
+lib,
+# You also have access to your flake's inputs.
+inputs,
+
+# All other arguments come from NixPkgs. You can use `pkgs` to pull packages or helpers
+# programmatically or you may add the named attributes as arguments here.
+pkgs, stdenv, ... }: 
+
+pkgs.iosevka.override {
+  privateBuildPlan = builtins.readFile ./build-plans.toml;
+  set = "josevka";
+}
