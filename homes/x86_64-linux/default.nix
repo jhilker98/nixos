dcc4ebc5558dd8c9067317f5ce5b3f6a869e8aaf
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-06 16:43:03 -0400

    restarting homes

--- homes/x86_64-linux/default.nix
+++ homes/x86_64-linux/default.nix
@@ -2,12 +2,23 @@
   lib,
   pkgs,
   inputs,
-  home,
-  target,
-  format,
-  virtual,
-  host,
   config,
   ...
-}: {
-} 
+}: 
+with lib;
+with lib.jhilker; 
+let cfg = lib.jhilker.common;
+in {
+  options.jhilker.common = {
+    enable = mkEnableOption "Common";
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      stateVersion = "23.11";
+    };
+
+    programs = {
+      home-manager.enable = true;
+    };
+};
--- homes/x86_64-linux/jhilker@wsl/default.nix
+++ homes/x86_64-linux/jhilker@wsl/default.nix
@@ -7,12 +7,6 @@
     # You also have access to your flake's inputs.
     inputs,
 
-    # Additional metadata is provided by Snowfall Lib.
-    home, # The home architecture for this host (eg. `x86_64-linux`).
-    target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
-    format, # A normalized name for the home target (eg. `home`).
-    virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
-    host, # The host name for this home.
 
     # All other arguments come from the home home.
     config,
