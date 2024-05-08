cdaa1eee988d7c911fd67ca69d569927f6d019fc
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-08 14:37:42 -0400

    added emacs module

--- modules/home/common/default.nix
+++ modules/home/common/default.nix
@@ -7,22 +7,65 @@ let
 
   cfg = config.jhilker98.common;
 in {
-  options.jhilker98.common = {
-    enable = mkEnableOption "Common";
+  options.jhilker98 = {
+    common = { enable = mkEnableOption "Common"; };
+    development = { enable = mkEnableOption "Development"; };
   };
 
   config = mkIf cfg.enable {
     home = {
       stateVersion = "23.11";
-      username = "jhilker"; 
-      packages = with pkgs; [];
+      username = "jhilker";
+      packages = with pkgs; [
+        texlive.combined.scheme-full
+        qpdf
+        cheat
+        auctex
+        zlib
+        (python3.withPackages (p:
+          with p; [
+            fontforge
+            numpy
+            pandas
+            flask
+            virtualenvwrapper
+            pip
+            # httpx
+            pygobject3
+          ]))
+        act
+        tldr
+        wakatime
+        ttfautohint
+        nodePackages.pyright
+        nodejs
+        nodePackages.npm
+        nodePackages.tailwindcss
+        nodePackages.postcss-cli
+        nodePackages.typescript
+        nodePackages.degit
+        nodePackages.postcss
+        rustc
+        cargo
+        go
+        thefuck
+        jq
+        pup
+        #libby
+        hugo
+        nix-prefetch-git
+        ranger
+        nodePackages.pnpm
+        graphite-cli
+        bitwarden-cli
+        nixfmt
+        plantuml
+        mermaid-cli
+        units
+      ];
     };
 
-    programs = {
-      home-manager.enable = true;
-    };
-
-    
+    programs = { home-manager.enable = true; };
 
     xdg.enable = true;
   };
--- modules/home/development/emacs/default.nix
+++ modules/home/development/emacs/default.nix
@@ -4,6 +4,18 @@ with lib.jhilker98;
   let
     cfg = lib.jhilker98.development.emacs;
   in {
+    options.jhilker98.emacs = {
+      enable = mkEnableOption "Emacs"; 
+    }; 
+    config = mkIf cfg.enable {
+      programs.emacs = {
+        enable = true;
+        package = pkgs.emacs29;
+      }
+      services.emacs = {
+        enable = true;
+      };
 
-  };
+    }
+  }
 
