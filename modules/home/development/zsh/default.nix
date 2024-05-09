d65daba03433b0615632db7a408bac4a099c74be
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-09 14:08:45 -0400

    added autocd

--- modules/home/development/zsh/default.nix
+++ modules/home/development/zsh/default.nix
@@ -1,19 +1,23 @@
-{lib, config, pkgs, inputs, ...}:
+{ lib, config, pkgs, inputs, ... }:
+
 with lib;
 with lib.jhilker98;
 let
   cfg = config.jhilker98.zsh;
 in {
   options.jhilker98.zsh = {
-	enable = mkEnableOption "ZSH";
-  };
-  config = mkIf cfg.enable {
-	programs.zsh = {
-	  enable = true;
-	  syntaxHighlighting.enable = true;
-	  autocd.enable = true;
-	  autosuggestion.enable = true;
-	};
+    enable = mkEnableOption "Zsh";
   };
 
+  config = mkIf cfg.enable {
+    programs = {
+      zsh = {
+        enable = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+	autocd = true;
+        };
+    };
+
+  };
 }
