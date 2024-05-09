7d3efbc43f209071d12c1c582df5017a6cc4c499
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-09 13:27:27 -0400

    added default nix config

---
+++ homes/x86_64-linux/jhilker/default.nix
@@ -0,0 +1,21 @@
+{
+    # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+    # as well as the libraries available from your flake's inputs.
+    lib,
+    # An instance of `pkgs` with your overlays and packages applied is also available.
+    pkgs,
+    # You also have access to your flake's inputs.
+    inputs,
+
+
+    # All other arguments come from the home home.
+    config,
+    ...
+}:
+with lib.jhilker98;
+{
+  jhilker98 = {
+    common.enable = true;
+    development.enable = true;
+  };
+}
