60069f0d07acafcaad9f1f6282eb5ec7fe85b248
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-10 16:12:54 -0400

    added nixvim to modules and home

--- homes/x86_64-linux/jhilker/default.nix
+++ homes/x86_64-linux/jhilker/default.nix
@@ -18,5 +18,6 @@
     common.enable = true;
     development.enable = true;
     git.enable = true;
+    nixvim.enable = true;
   };
 }
--- modules/home/development/nixvim/default.nix
+++ modules/home/development/nixvim/default.nix
@@ -4,24 +4,35 @@ with lib;
 with lib.jhilker98;
 let cfg = config.jhilker98.nixvim;
 in {
-  options.jhilker98.nixvim = {
-   enable = mkEnableOption "Nixvim";
-  };
+  options.jhilker98.nixvim = { enable = mkEnableOption "Nixvim"; };
   config = mkIf cfg.enable {
     programs = {
       nixvim = {
         enable = true;
         defaultEditor = true;
+        options = {
+          number = true; # Show line numbers
+          relativenumber = true; # Show relative line numbers
+          shiftwidth = 2; # Tab width should be 2
+          termguicolors = true;
+        };
+        extraPlugins = with pkgs.vimPlugins; [
+          vim-nix
+          nvim-fzf
+          nvim-fzf-commands
+        ];
+
+        globals = { mapleader = " "; };
+        plugins = {
+          fugitive.enable = true;
+          emmet.enable = true;
+          which-key = { enable = true; };
+	  lightline.enable = true;
+        };
       };
-      zsh.shellAliases = {
-        "vim" = "nvim";
-      };
-      bash.shellAliases = {
-        "vim" = "nvim";
-      };
-      fish.shellAliases = {
-        "vim" = "nvim";
-      };
+      zsh.shellAliases = { "vim" = "nvim"; };
+      bash.shellAliases = { "vim" = "nvim"; };
+      fish.shellAliases = { "vim" = "nvim"; };
     };
   };
 }
