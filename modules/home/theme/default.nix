b2b144bb7d2a974f15f6f72d806b2c59237ee4a2
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-10 17:08:32 -0400

    fixed wallpaper error

--- modules/home/theme/default.nix
+++ modules/home/theme/default.nix
@@ -11,7 +11,11 @@ in {
 
   config = mkIf cfg.enable {
     stylix = {
-      base16Scheme = "${inputs.base16-themes}/share/gruvbox-dark-hard.yaml";
+      image = pkgs.fetchurl {
+        url = "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/YRAJQAT4Dg-avesta-rezaeizadeh-unsplash.jpg";
+        sha256 = "06n1dlkdhz5c287b72aplg3z90a3rixy79ryyjskmdfqy283r1an";
+      };
+      base16Scheme = "${inputs.base16-themes}/base16/gruvbox-dark-hard.yaml";
     };
   };
 }
