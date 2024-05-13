cd871870c3dfd0298cf08687b168690d3371d62e
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-12 21:58:21 -0400

    always have bookmarks enabled

--- modules/home/development/fzf/default.nix
+++ modules/home/development/fzf/default.nix
@@ -3,27 +3,27 @@ with lib;
 with lib.jhilker98;
 let cfg = config.jhilker98.development.fzf;
 in {
-  options.jhilker98.development.fzf = {
-    enable = mkEnableOption "FZF";
-    bookmarks = { enable = mkEnableOption "FZF-marks"; };
-  };
+  options.jhilker98.development.fzf = { enable = mkEnableOption "FZF"; };
 
   config = mkIf cfg.enable {
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = mkIf config.jhilker98.zsh.enable;
-      enableBashIntegration = mkIf config.jhilker98.bash.enable;
-      enableFishIntegration = mkIf config.jhilker98.fish.enable;
+    programs = {
+      fzf = {
+        enable = true;
+        enableZshIntegration = mkIf config.jhilker98.zsh.enable;
+        enableBashIntegration = mkIf config.jhilker98.bash.enable;
+        enableFishIntegration = mkIf config.jhilker98.fish.enable;
+      };
+      bash.initExtra = ''
+        source ${inputs.fzf-marks}/fzf-marks.plugin.zsh
+      '';
+      zsh.initExtra = ''
+        source ${inputs.fzf-marks}/fzf-marks.plugin.zsh
+      '';
+
+      fish.initExtra = ''
+        source ${inputs.fzf-marks}/conf.d/fzf-marks.plugin.fish
+      '';
     };
-    programs.bash.initExtra = ''
-      source ${inputs.fzf-marks}/fzf-marks.plugin.bash
-    '';
-    programs.zsh.initExtra = ''
-      source ${inputs.fzf-marks}/fzf-marks.plugin.zsh
-    '';
-    programs.fish.interactiveShellInit = ''
-      source ${inputs.fzf-marks}/conf.d/fzf-marks.plugin.fish
-    '';
   };
 
 }
