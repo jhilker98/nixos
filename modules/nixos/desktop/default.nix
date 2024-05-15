2e342f84e74bee608e0c8c5dd024719cfc338ea0
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-15 12:42:36 -0400

    started desktop module

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -2,8 +2,10 @@
 
 with lib;
 with lib.jhilker98;
-let
-  cfg = config.jhilker98.nixos.desktop;
+let cfg = config.jhilker98.nixos.desktop;
 in {
-
+  options.jhilker98.nixos.desktop = {
+    enable = mkEnableOption "Desktop";
+    # useWayland =
+  };
 }
