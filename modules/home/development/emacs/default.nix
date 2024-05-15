6506a9e2fc5e95cf651e7b6a2ce6445fa07080c5
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-15 12:22:56 -0400

    added doom emacs dependencies

--- modules/home/development/emacs/default.nix
+++ modules/home/development/emacs/default.nix
@@ -1,25 +1,36 @@
-{config, lib, pkgs, ...}:
+{ config, lib, pkgs, ... }:
 with lib;
 with lib.jhilker98;
-  let
-    cfg = config.jhilker98.development.emacs;
-  in {
-    options.jhilker98.development.emacs = {
-      enable = mkEnableOption "Emacs"; 
-    }; 
-    config = mkIf cfg.enable {
-      programs.emacs = {
-        enable = true;
-        package = pkgs.emacs29;
-      };
-      services.emacs = {
-        enable = true;
-      };
-      xdg.configFile."doom" = {
-        source = ./config;
-	recursive = true;
-      };
-	home.sessionVariables."DOOMDIR" = "$HOME/.dotfiles/modules/home/development/emacs/config";
+let cfg = config.jhilker98.development.emacs;
+in {
+  options.jhilker98.development.emacs = { enable = mkEnableOption "Emacs"; };
+  config = mkIf cfg.enable {
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs29;
     };
-  }
-
+    services.emacs = { enable = true; };
+    xdg.configFile."doom" = {
+      source = ./config;
+      recursive = true;
+    };
+    home = {
+      sessionVariables."DOOMDIR" =
+        "$HOME/.dotfiles/modules/home/development/emacs/config";
+      packages = with pkgs; [
+        gcc
+        binutils
+        (ripgrep.override { withPCRE2 = true; })
+        gnutls
+        gnumake
+        fd
+        imagemagick
+        zstd
+        nodePackages.javascript-typescript-langserver
+        sqlite
+        editorconfig-core-c
+        emacs-all-the-icons-fonts
+      ];
+    };
+  };
+}
