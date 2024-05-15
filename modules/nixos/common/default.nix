6138a6d76226947156756d432aa02478fcef78b2
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-15 13:45:14 -0400

    added user config and git hook

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -2,13 +2,28 @@
 
 with lib;
 with lib.jhilker98;
-let
-  cfg = config.jhilker98.nixos.common;
+let cfg = config.jhilker98.nixos.common;
 in {
   options.jhilker98.nixos.common = {
     enable = mkEnableOption "Core NixOS module";
   };
   config = mkIf cfg.enable {
     system.stateVersion = "23.11";
+    services = { account-daemon.enable = true; };
+    users = {
+      defaultUserShell = pkgs.zsh;
+      users = {
+        jhilker = {
+          isNormalUser = true;
+          isSystemUser = false;
+          extraGroups =
+            [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+          initialPassword = "jhilker";
+          shell = pkgs.fish;
+          home = "/home/jhilker";
+        createHome = "false";
+        };
+      };
+    };
   };
 }
