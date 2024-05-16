4ce8a1b5a5e983292f93b4b60628847daf08ac47
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-15 20:11:00 -0400

    added gi function to fish

--- modules/home/cli/fish/default.nix
+++ modules/home/cli/fish/default.nix
@@ -1,15 +1,18 @@
-{config, lib, pkgs, inputs, ...}:
+{ config, lib, pkgs, inputs, ... }:
 with lib;
 with lib.jhilker98;
-let
-  cfg = config.jhilker98.fish;
+let cfg = config.jhilker98.fish;
 in {
-  options.jhilker98.fish = {
-    enable = mkEnableOption "Fish";
-  };
+  options.jhilker98.fish = { enable = mkEnableOption "Fish"; };
   config = mkIf cfg.enable {
     programs.fish = {
       enable = true;
+      functions = {
+        "gi" = ''
+          set toIgnore $(curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/list | sed 's/,/\n/g' | fzf -m | xargs | sed 's/\s/,/g')
+                  curl -sL "https://www.toptal.com/developers/gitignore/api/$toIgnore" >> .gitignore
+        '';
+      };
     };
   };
 }
