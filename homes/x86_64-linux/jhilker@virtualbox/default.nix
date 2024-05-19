c3e748dfcefa53494c360ac1e08c6e5106049165
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-19 15:45:20 -0400

    started home for virtualbox

--- homes/x86_64-linux/jhilker@virtualbox/default.nix
+++ homes/x86_64-linux/jhilker@virtualbox/default.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-
+  jhilker98 = {
+    common.enable = true;
+    development = { emacs.enable = false; };
+    langs = { latex.enable = false; };
+    git.enable = true;
+  };
 }
