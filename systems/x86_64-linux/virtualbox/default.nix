a4dbb209fb04fdce616e60c9f5b7be9c7074840e
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-20 16:23:36 -0400

    added virtualbox hardware config

--- systems/x86_64-linux/virtualbox/default.nix
+++ systems/x86_64-linux/virtualbox/default.nix
@@ -1,8 +1,8 @@
 { lib, pkgs, inputs, system, target, format, virtual, systems, config, ... }: {
 
-#  imports = [
-#    ./disk-configuration.nix
-#  ];
+  #  imports = [
+  #    ./disk-configuration.nix
+  #  ];
   networking.hostName = "virtualbox";
   boot.loader = {
     grub = {
@@ -17,4 +17,17 @@
     };
   };
 
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  boot.initrd.availableKernelModules =
+    [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+  swapDevices = [ ];
+  virtualisation.virtualbox.guest.enable = true;
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
