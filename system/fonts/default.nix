69a8447614257b686b71d2b80accb922d7a906ba
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-20 14:49:32 -0400

    removed symbols-nerd-font

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -27,36 +27,36 @@
       noto-fonts
       twemoji-color-font
 
-      (stdenv.mkDerivation {
-        pname = "symbols-nerd-font";
-        version = "2.2.0";
-        src = pkgs.fetchFromGitHub {
-          owner = "ryanoasis";
-          repo = "nerd-fonts";
-          rev = "a07648b1eef52c87670fcd5b567c55493c0b3205"; ## nerd_font_symbols
-          sha256 = "1g60wi07awxliq9gfypsvp2wjgpg7qz6k1k7iph7iqmjydan3b9k"; ## nerd_font_symbols
-          sparseCheckout = [
-            "10-nerd-font-symbols.conf"
-            "patched-fonts/NerdFontsSymbolsOnly"
-          ];
-        };
-        dontConfigure = true;
-        dontBuild = true;
-        installPhase = ''
-          runHook preInstall
+      ##(stdenv.mkDerivation {
+      ##  pname = "symbols-nerd-font";
+      ##  version = "2.2.0";
+      ##  src = pkgs.fetchFromGitHub {
+      ##    owner = "ryanoasis";
+      ##    repo = "nerd-fonts";
+      ##    rev = "a07648b1eef52c87670fcd5b567c55493c0b3205"; ## nerd_font_symbols
+      ##    sha256 = "1g60wi07awxliq9gfypsvp2wjgpg7qz6k1k7iph7iqmjydan3b9k"; ## nerd_font_symbols
+      ##    sparseCheckout = [
+      ##      "10-nerd-font-symbols.conf"
+      ##      "patched-fonts/NerdFontsSymbolsOnly"
+      ##    ];
+      ##  };
+      ##  dontConfigure = true;
+      ##  dontBuild = true;
+      ##  installPhase = ''
+      ##    runHook preInstall
 
-          fontconfigdir="$out/etc/fonts/conf.d"
-          install -d "$fontconfigdir"
-          install 10-nerd-font-symbols.conf "$fontconfigdir"
+      ##    fontconfigdir="$out/etc/fonts/conf.d"
+      ##    install -d "$fontconfigdir"
+      ##    install 10-nerd-font-symbols.conf "$fontconfigdir"
 
-          fontdir="$out/share/fonts/truetype"
-          install -d "$fontdir"
-          install "patched-fonts/NerdFontsSymbolsOnly/complete/Symbols-2048-em Nerd Font Complete.ttf" "$fontdir"
+      ##    fontdir="$out/share/fonts/truetype"
+      ##    install -d "$fontdir"
+      ##    install "patched-fonts/NerdFontsSymbolsOnly/complete/Symbols-2048-em Nerd Font Complete.ttf" "$fontdir"
 
-          runHook postInstall
-        '';
-        enableParallelBuilding = true;
-      })
+      ##    runHook postInstall
+      ##  '';
+      ##  enableParallelBuilding = true;
+      ##})
 
 
     ];
