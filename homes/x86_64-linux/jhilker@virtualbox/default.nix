33b784f7bc77de210ced39d11fd3486237baf56b
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-19 20:02:58 -0400

    removed python

--- homes/x86_64-linux/jhilker@virtualbox/default.nix
+++ homes/x86_64-linux/jhilker@virtualbox/default.nix
@@ -4,7 +4,11 @@
   jhilker98 = {
     common.enable = true;
     development = { emacs.enable = lib.mkDefault false; };
-    langs = { latex.enable = lib.mkDefault false; };
+    langs = {
+      latex.enable = lib.mkDefault false;
+      python.enable = lib.mkDefault false;
+    };
+
     git.enable = true;
   };
 }
