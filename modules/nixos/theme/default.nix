b08c627a7079c618595aad06c8b176d8bd61ac81
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-21 13:00:16 -0400

    restarted nixos theme config

--- modules/nixos/theme/default.nix
+++ modules/nixos/theme/default.nix
@@ -4,5 +4,10 @@ with lib.jhilker98;
 let
   cfg = config.jhilker98.nixos.theme;
 in {
+  options.jhilker98.nixos.theme = {
+    enable = mkEnableOption  "enable theme";
+  };
+  config = mkIf cfg.enable {
 
+  };
 }
