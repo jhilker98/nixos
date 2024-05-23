cdf6307724b2a3e4fa49dd9a1adbb276c3b84a48
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-23 12:53:35 -0400

    added alacritty module

---
+++ modules/home/apps/alacritty/default.nix
@@ -0,0 +1,12 @@
+{config, lib, pkgs, inputs, ...}:
+with lib;
+with lib.jhilker98; 
+let cfg = config.jhilker98.apps.alacritty; 
+in {
+  options = {
+    enable = mkEnableOption "Enable alacritty terminal";
+  };
+  config = mkIf cfg.enable {
+
+  };
+}
