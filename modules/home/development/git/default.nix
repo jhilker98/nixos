8a78b94a66c7d6dcd0d6edc6b96f670a40ac4645
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-23 22:20:22 -0400

    added safe-directory to git

--- modules/home/development/git/default.nix
+++ modules/home/development/git/default.nix
@@ -2,12 +2,9 @@
 
 with lib;
 with lib.jhilker98;
-let
-  cfg = config.jhilker98.git;
+let cfg = config.jhilker98.git;
 in {
-  options.jhilker98.git = {
-    enable = mkEnableOption "Git";
-  };
+  options.jhilker98.git = { enable = mkEnableOption "Git"; };
 
   config = mkIf cfg.enable {
     programs.git = {
@@ -21,19 +18,20 @@ in {
       aliases = {
         "cleanup" =
           "grep  -v '\\*\\|master\\|develop\\|dev\\|main' | xargs -n 1 -r git branch -d";
+      };
+      delta.enable = true;
+      extraConfig = {
+        init = { defaultBranch = "main"; };
+        core = { editor = "nvim"; };
+        push = { autoSetupRemote = true; };
+        color = { ui = true; };
+        format = {
+          pretty =
+            "%C(italic)(%h)%Creset %C(bold 12)%an%Creset: %C(3)%s %Creset(%ad)";
         };
-        delta.enable = true;
-        extraConfig = {
-          init = { defaultBranch = "main"; };
-          core = { editor = "nvim"; };
-          push = { autoSetupRemote = true; };
-          color = { ui = true; };
-          format = {
-            pretty =
-              "%C(italic)(%h)%Creset %C(bold 12)%an%Creset: %C(3)%s %Creset(%ad)";
-            };
-          };
-          ignores = [ "result" "result-*" "node_modules/"  "*~" "*.swp" ];
-        };
+        safe.directory = "*";
+      };
+      ignores = [ "result" "result-*" "node_modules/" "*~" "*.swp" ];
     };
+  };
 }
