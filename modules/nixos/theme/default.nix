2cfcd726c60eadbbf5c48e073aeaeabbec5c96b6
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-25 11:37:55 -0400

    changed wallpaper

--- modules/nixos/theme/default.nix
+++ modules/nixos/theme/default.nix
@@ -1,16 +1,18 @@
-{config, lib, pkgs, inputs, ...}:
+{ config, lib, pkgs, inputs, ... }:
 with lib;
 with lib.jhilker98;
-let
-  cfg = config.jhilker98.theme;
+let cfg = config.jhilker98.theme;
 in {
-  options.jhilker98.theme = {
-    enable = mkEnableOption  "enable theme module";
-  };
+  options.jhilker98.theme = { enable = mkEnableOption "enable theme module"; };
   config = mkIf cfg.enable {
-    stylix.image = pkgs.fetchurl {
-    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-    sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-  };
+    stylix = {
+      image = pkgs.fetchurl {
+        url =
+          "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/YRAJQAT4Dg-avesta-rezaeizadeh-unsplash.jpg";
+        sha256 = "06n1dlkdhz5c287b72aplg3z90a3rixy79ryyjskmdfqy283r1an";
+      };
+      base16Scheme =
+        "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    };
   };
 }
