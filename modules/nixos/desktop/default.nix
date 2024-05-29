a367935c783db707a273cdf9f29829df4b4f6e5b
Jacob Hilker <jacob.hilker2@gmail.com>
2024-05-28 20:30:32 -0400

    removed picom package

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -35,14 +35,14 @@ in {
     };
     services.picom = mkIf cfg.usePicom {
       enable = true;
-      package = pkgs.picom.overrideAttrs (old: {
-        src = pkgs.fetchFromGitHub {
-          owner = "jonaburg";
-          repo = "picom";
-          rev = "e3c19cd7d1108d114552267f302548c113278d45";
-          sha256 = "19nglw72mxbr47h1nva9fabzjv51s4fy6s1j893k4zvlhw0h5yp2";
-        };
-      });
+      #package = pkgs.picom.overrideAttrs (old: {
+      #  src = pkgs.fetchFromGitHub {
+      #    owner = "jonaburg";
+      #    repo = "picom";
+      #    rev = "e3c19cd7d1108d114552267f302548c113278d45";
+      #    sha256 = "19nglw72mxbr47h1nva9fabzjv51s4fy6s1j893k4zvlhw0h5yp2";
+      #  };
+      #});
     };
     environment.systemPackages = with pkgs; [
       libsForQt5.qt5.qtgraphicaleffects
