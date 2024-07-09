1e044a756a7e4bd190842c7051309414f9a9dcf6
Jacob Hilker <jacob.hilker2@gmail.com>
2024-07-08 21:29:53 -0400

    got paisa working

--- modules/home/apps/ledger/default.nix
+++ modules/home/apps/ledger/default.nix
@@ -2,6 +2,7 @@
 with lib;
 with lib.jhilker98;
 let cfg = config.jhilker98.apps.ledger;
+  currentSys = pkgs.system;
 in {
   options.jhilker98.apps.ledger = {
     enable = mkEnableOption "Ledger";
@@ -10,6 +11,7 @@ in {
     home.packages = [
       pkgs.ledger
       pkgs.beancount
+      inputs.paisa.packages.${currentSys}.default
     ];
   };
 }
