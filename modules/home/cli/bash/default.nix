25747e5820669888e72b82c6f7aceaaefe748f50
Jacob Hilker <jacob.hilker2@gmail.com>
2024-07-23 12:32:08 -0400

    added mkGitIgnore to both zsh and bash

--- modules/home/cli/bash/default.nix
+++ modules/home/cli/bash/default.nix
@@ -2,6 +2,10 @@
 with lib;
 with lib.jhilker98;
 let cfg = config.jhilker98.bash;
+mkGitIgnore = ''
+  toIgnore=$(curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/list | sed 's/,/\n/g' | fzf -m | xargs | sed 's/\s/,/g')
+    curl -sL "https://www.toptal.com/developers/gitignore/api/$toIgnore" >> .gitignore
+        '';
 in {
   options.jhilker98.bash.enable = mkEnableOption "Bash";
   config = mkIf cfg.enable {
@@ -13,6 +17,7 @@ in {
           ls = "${pkgs.eza}/bin/eza -alh --group-directories-first";
           cat = "${pkgs.bat}/bin/bat -p";
         };
+
       };
     };
   };
--- modules/home/cli/zsh/default.nix
+++ modules/home/cli/zsh/default.nix
@@ -3,6 +3,11 @@
 with lib;
 with lib.jhilker98;
 let cfg = config.jhilker98.zsh;
+mkGitIgnore = ''
+  toIgnore=$(curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/list | sed 's/,/\n/g' | fzf -m | xargs | sed 's/\s/,/g')
+    curl -sL "https://www.toptal.com/developers/gitignore/api/$toIgnore" >> .gitignore
+        '';
+
 in {
   options.jhilker98.zsh = { enable = mkEnableOption "Zsh"; };
 
