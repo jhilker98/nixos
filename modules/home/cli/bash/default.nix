d875e3e4508474879f6bb10d68118f5b653448ba
Jacob Hilker <jacob.hilker2@gmail.com>
2024-07-28 13:45:57 -0400

    re-added ref alias

--- modules/home/cli/bash/default.nix
+++ modules/home/cli/bash/default.nix
@@ -1,18 +1,19 @@
 { config, lib, pkgs, inputs, ... }:
 with lib;
 with lib.jhilker98;
-let cfg = config.jhilker98.bash;
+let cfg = config.jhilker98.cli.bash;
 mkGitIgnore = ''
   toIgnore=$(curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/list | sed 's/,/\n/g' | fzf -m | xargs | sed 's/\s/,/g')
     curl -sL "https://www.toptal.com/developers/gitignore/api/$toIgnore" >> .gitignore
         '';
 in {
-  options.jhilker98.bash.enable = mkEnableOption "Bash";
+  options.jhilker98.cli.bash.enable = mkEnableOption "Bash";
   config = mkIf cfg.enable {
     programs = {
       bash = {
         enable = true;
         shellAliases = {
+          ref = "source ~/.bashrc";
           ls = "${pkgs.eza}/bin/eza -alh --group-directories-first";
           cat = "${pkgs.bat}/bin/bat -p";
         };
