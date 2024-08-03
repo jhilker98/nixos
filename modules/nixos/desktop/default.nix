5206f888d6ba7a12058bd6c73f413b2c0b1523af
Jacob Hilker <jacob.hilker2@gmail.com>
2024-08-03 14:44:11 -0400

    removed backend from desktop/default.nix

--- modules/nixos/desktop/default.nix
+++ modules/nixos/desktop/default.nix
@@ -18,7 +18,6 @@ in {
         windowManager = {
           qtile = {
             enable = true;
-            backend = "x11";
             extraPackages = python3Packages:
               with python3Packages;
               [ qtile-extras ];
