d9beeece895f3eeb81975f92c15d8c8aaea46785
Jacob Hilker <jacob.hilker2@gmail.com>
2024-11-25 14:09:34 -0500

    mod: create nixos modules

---
+++ modules/nixos/common/default.nix
@@ -0,0 +1,85 @@
+{ lib, config, pkgs, inputs, ... }:
+
+with lib;
+with lib.jhilker98;
+
+let cfg = config.jhilker98.common;
+in {
+  options.jhilker98.common = { enable = mkEnableOption "Common"; };
+
+  config = mkIf cfg.enable {
+
+    system.stateVersion = "23.11";
+    nix = {
+      package = pkgs.nixFlakes;
+      settings = {
+        #extraExperimentalFeaatures = ["nix-command" "flakes"];
+        auto-optimise-store = true;
+      };
+      gc = {
+        automatic = true;
+        dates = "daily";
+      };
+      optimise.automatic = true;
+    };
+    home-manager.backupFileExtension = ".bak";
+    users = {
+      defaultUserShell = pkgs.zsh;
+      users = {
+        jhilker = {
+          isNormalUser = true;
+          isSystemUser = false;
+          extraGroups =
+            [ "wheel" "networkmanager" "audio" ]; # Enable ‘sudo’ for the user.
+          initialPassword = "jhilker";
+          shell = pkgs.fish;
+          home = "/home/jhilker";
+        };
+      };
+    };
+    environment.systemPackages = with pkgs; [ sops mkpasswd whois age];
+    programs = {
+      git = {
+        enable = true;
+        config = { safe.directory = "*"; };
+      };
+      neovim = {
+        enable = true;
+        viAlias = true;
+        vimAlias = true;
+      };
+      gnupg.agent = {
+        enable = true;
+        enableSSHSupport = true;
+        # pinentryFlavor = "gtk2";
+        pinentryPackage = pkgs.pinentry-gtk2;
+      };
+      zsh.enable = true;
+      fish.enable = true;
+      dconf.enable = true;
+    };
+    security = {
+      sudo = {
+        enable = true;
+      };
+      #doas = {
+      #  enable = true;
+      #  extraRules = [{
+      #    users = [ "jhilker" ];
+      #    keepEnv = true;
+      #    persist = true;
+      #  }];
+      pam.services = { sddm.enableKwallet = true; };
+      polkit.enable = true;
+    };
+    system.activationScripts = {
+        base-dirs = {
+          text = ''
+            mkdir -p /nix/var/nix/profiles/per-user/jhilker
+          '';
+          deps = [ ];
+        };
+      };
+  };
+
+}
---
+++ modules/nixos/theme/default.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, inputs, ... }:
+with lib;
+with lib.jhilker98;
+let cfg = config.jhilker98.theme;
+in {
+  options.jhilker98.theme = { enable = mkEnableOption "enable theme module"; };
+  config = mkIf cfg.enable {
+    stylix = {
+      image = pkgs.fetchurl {
+        url =
+          "https://gruvbox-wallpapers.pages.dev/wallpapers/irl/YRAJQAT4Dg-avesta-rezaeizadeh-unsplash.jpg";
+        sha256 = "06n1dlkdhz5c287b72aplg3z90a3rixy79ryyjskmdfqy283r1an";
+      };
+        targets = {
+          grub.enable = true;
+      };
+    };
+  };
+}
