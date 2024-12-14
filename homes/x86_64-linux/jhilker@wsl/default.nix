e002fca18b2dc17f1a7348c274b812c22b8ae6af
Jacob Hilker <jacob.hilker2@gmail.com>
2024-12-14 15:00:21 -0500

    home: add zsh and git to wsl

--- homes/x86_64-linux/jhilker@wsl/default.nix
+++ homes/x86_64-linux/jhilker@wsl/default.nix
@@ -3,6 +3,11 @@ with lib.jhilker98; {
   jhilker98 = {
     common.enable = true;
     calendar.enable = true;
+    git.enable = true;
+    cli = {
+      starship.enable = true;
+      zsh.enable = true;
+    };
   };
 
 
