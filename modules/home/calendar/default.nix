1f9bb5620e22d7fdea2b813287ab906af11c51b9
Jacob Hilker <jacob.hilker2@gmail.com>
2024-12-16 14:00:09 -0500

    etc: remove gcalclirc from cal mod

--- modules/home/calendar/default.nix
+++ modules/home/calendar/default.nix
@@ -1,15 +1,8 @@
-{config, lib, pkgs, inputs, ...}:
+{ config, lib, pkgs, inputs, ... }:
 with lib;
 with lib.jhilker98;
-let
-cfg = config.jhilker98.calendar;
+let cfg = config.jhilker98.calendar;
 in {
-options.jhilker98.calendar = { enable = mkEnableOption "calendar"; };
-config = mkIf cfg.enable {
-  home.packages = with pkgs; [ gcalcli ];
-  home.file.".gcalclirc".text = ''
-    --client-id=${config.sops.secrets."gcal/client_id"}
-    --client-secret=${config.sops.secrets."gcal/client_secret"}
-  '';
-};
+  options.jhilker98.calendar = { enable = mkEnableOption "calendar"; };
+  config = mkIf cfg.enable { home.packages = with pkgs; [ gcalcli ]; };
 }
