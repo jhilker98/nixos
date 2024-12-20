9d9524c41b89fb7828632e117e8894c130d0bbd5
Jacob Hilker <jacob.hilker2@gmail.com>
2024-12-20 15:35:08 -0500

    etc: remove import from wsl

--- homes/x86_64-linux/jhilker@wsl/default.nix
+++ homes/x86_64-linux/jhilker@wsl/default.nix
@@ -1,8 +1,5 @@
 {lib, config, pkgs, inputs, username ? "camoh", ...}:
 with lib.jhilker98; {
-  imports = [
-    inputs.sops-nix.homeManagerModules.sops
-  ];
   jhilker98 = {
     common.enable = true;
     calendar.enable = true;
