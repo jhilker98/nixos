1dac2baed270e10c76ff88c1b897ed8a52087297
Jacob Hilker <jacob.hilker2@gmail.com>
2024-12-21 19:33:46 -0500

    mod: add secrets to calendar module

--- modules/home/calendar/default.nix
+++ modules/home/calendar/default.nix
@@ -5,12 +5,18 @@ let cfg = config.jhilker98.calendar;
 in {
   options.jhilker98.calendar = { enable = mkEnableOption "calendar"; };
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ gcalcli ];
+    home.packages = with pkgs; [ gcalcli calcurse ];
     sops.secrets = {
       "gcal/client_id" = { };
       "gcal/client_secret" = { };
     };
-
+    xdg.configFile."gcalcli/config.toml".source =
+      (pkgs.formats.toml { }).generate "config" {
+        auth = {
+          client_id = config.sops.secrets."gcal/client_id".path;
+          client_secret = config.sops.secrets."gcal/client_secret".path;
+        };
+      };
   };
 
 }
