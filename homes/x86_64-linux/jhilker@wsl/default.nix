b1d3412888210df4275318411be523398e6497e3
Jacob Hilker <jacob.hilker2@gmail.com>
2024-12-24 13:11:05 -0500

    home: finish creating windows home

--- homes/x86_64-linux/jhilker@wsl/default.nix
+++ homes/x86_64-linux/jhilker@wsl/default.nix
@@ -16,4 +16,73 @@ with lib.jhilker98; {
     };
   };
 
+  home = {
+    sessionVariables = {
+      "WSLHOME" = "/mnt/c/Users/camoh";
+      "VIRTUALENVWRAPPER_PYTHON" = "/home/jhilker/.nix-profile/bin/python3";
+    };
+    packages = with pkgs; [
+      qpdf
+      zlib
+      wakatime
+      ttfautohint
+      nodejs
+      nodePackages.npm
+      nodePackages.tailwindcss
+      nodePackages.postcss-cli
+      nodePackages.typescript
+      nodePackages.degit
+      nodePackages.postcss
+      rustc
+      cargo
+      go
+      jq
+      pup
+      #libby
+      hugo
+      nix-prefetch-git
+      ranger
+      nodePackages.pnpm
+      graphite-cli
+      bitwarden-cli
+      iamb
+      jq
+      (pkgs.writers.writePython3Bin "rdate" { libraries = [ ]; } ''
+        import random as r
+        import argparse
+        parser = argparse.ArgumentParser(description="Generates a random date")
+        parser.add_argument("-y",
+                            "--year",
+                            help="Generate a date in a particular year",
+                            action='store', dest='year', default="")
+
+        parser.add_argument("-n",
+                            "--number",
+                            help="Generate a certain number of dates",
+                            action='store', dest='number', default=1)
+        args = parser.parse_args()
+        dates = {
+                "Jan": 31,
+                "Feb": 28,
+                "Mar": 31,
+                "Apr": 30,
+                "May": 31,
+                "Jun": 30,
+                "Jul": 31,
+                "Aug": 31,
+                "Sep": 30,
+                "Oct": 31,
+                "Nov": 30,
+                "Dec": 31
+                }
+        year = vars(args)['year']
+        number = int(vars(args)['number'])
+        for num in range(number):
+            month = r.choice(list(dates.keys()))
+            day = r.randint(1, dates[month])
+            print(f'{day:02} {month} {year}')
+      '')
+    ];
+  };
+  programs.git.signing.gpgPath = "/mnt/c/Program Files (x86)/GnuPG/bin/gpg.exe";
 }
